95517a878c0f410790bd99830040af18b784c8a1
sungki.bae <opop4615@naver.com>
2023-04-20 17:53:59 +0900

    third

--- lib/screen/valuation.dart
+++ lib/screen/valuation.dart
@@ -12,10 +12,31 @@ class _ValuationScreenState extends State<ValuationScreen> {
 
   @override
   Widget build(BuildContext context) {
+    return TopBar();
+  }
+}
+
+class TopBar extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
     return Container(
-      child: Center(
-        child: Text("VALUATION"),
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        children: [
+          //Image.asset('asset/images/logo.png', fit: BoxFit.contain, height: 25,),
+          Text("모두의밸류에이션", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20, letterSpacing: -1.5),),
+        ],
       ),
+      
     );
   }
 }
+
+
+
+
+
+
+
+
+
