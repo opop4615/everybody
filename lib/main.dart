e46df070032f40420058a417954358aa42abaf13
sungki.bae <opop4615@naver.com>
2023-04-21 10:44:14 +0900

    230421

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
-import 'package:everyvaluation/screen/community.dart';
-import 'package:everyvaluation/screen/setting.dart';
-import 'package:everyvaluation/screen/valuation.dart';
+import 'package:everyvaluation/screen/home.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
-import 'package:motion_tab_bar_v2/motion-tab-bar.dart';
+import 'package:flutter_login/flutter_login.dart';
 
 void main() => runApp(const MyApp());
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -17,81 +17,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         brightness: Brightness.dark,
-        primaryColor:Color.fromARGB(255, 0, 0, 0),
+        primaryColor:const Color.fromARGB(255, 10, 38, 71),
         scaffoldBackgroundColor: const Color.fromARGB(255, 10, 38, 71),
         fontFamily: 'nanum'
       ),
-      home: const MyHomePage(title: '모두의 밸류에이션'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, this.title}) : super(key: key);
-
-  final String? title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  TabController? _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(
-      initialIndex: 0,
-      length: 3,
-      vsync: this,
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _tabController!.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: MotionTabBar(
-        initialSelectedTab: "가치평가",
-        useSafeArea: true,
-        labels: const ["가치평가", "커뮤니티", "설정"],
-        icons: const [Icons.calculate, Icons.home, Icons.settings],
-        tabSize: 50,
-        tabBarHeight: 55,
-        textStyle: const TextStyle(
-          fontSize: 12,
-          color: Colors.white,
-          fontWeight: FontWeight.w500,
-        ),
-        tabIconColor: Colors.white,
-        tabIconSize: 28.0,
-        tabIconSelectedSize: 26.0,
-        tabSelectedColor: const Color.fromARGB(255, 32, 82, 149),
-        tabIconSelectedColor: Colors.white,
-        tabBarColor: const Color.fromARGB(255, 20, 66, 114),
-        onTabItemSelected: (int value) {
-          setState(() {
-            _tabController!.index = value;
-          });
-        },
-      ),
-      body: TabBarView(
-        physics:
-            NeverScrollableScrollPhysics(), // swipe navigation handling is not supported
-        controller: _tabController,
-        // ignore: prefer_const_literals_to_create_immutables
-        children: <Widget>[
-          ValuationScreen(),
-          CommunityScreen(),
-          SettingScreen(),
-        ],
-      ),
+      home: MyHomePage(),
     );
   }
 }
---
+++ lib/screen/home.dart
@@ -0,0 +1,75 @@
+import 'package:everyvaluation/screen/community.dart';
+import 'package:everyvaluation/screen/setting.dart';
+import 'package:everyvaluation/screen/valuation.dart';
+import 'package:flutter/material.dart';
+import 'package:motion_tab_bar_v2/motion-tab-bar.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(
+      initialIndex: 0,
+      length: 3,
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController!.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: MotionTabBar(
+        initialSelectedTab: "가치평가",
+        useSafeArea: true,
+        labels: const ["가치평가", "커뮤니티", "설정"],
+        icons: const [Icons.calculate, Icons.home, Icons.settings],
+        tabSize: 50,
+        tabBarHeight: 55,
+        textStyle: const TextStyle(
+          fontSize: 12,
+          color: Colors.white,
+          fontWeight: FontWeight.w500,
+        ),
+        tabIconColor: Colors.white,
+        tabIconSize: 28.0,
+        tabIconSelectedSize: 26.0,
+        tabSelectedColor: const Color.fromARGB(255, 32, 82, 149),
+        tabIconSelectedColor: Colors.white,
+        tabBarColor: const Color.fromARGB(255, 20, 66, 114),
+        onTabItemSelected: (int value) {
+          setState(() {
+            _tabController!.index = value;
+          });
+        },
+      ),
+      body: TabBarView(
+        physics:
+            NeverScrollableScrollPhysics(), // swipe navigation handling is not supported
+        controller: _tabController,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: <Widget>[
+          ValuationScreen(),
+          CommunityScreen(),
+          SettingScreen(),
+        ],
+      ),
+    );
+  }
+}
